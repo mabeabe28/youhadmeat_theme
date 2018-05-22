f633fe931ffc387421a85d5d21fbe4dfe31babb6
Marion Abeabe <mabeabe28@gmail.com>
2018-05-22 16:18:33 +0100

    remove home page title

--- front-page.php
+++ front-page.php
@@ -288,13 +288,13 @@ get_header('home');
 		<?php
 		if ( have_posts() ) :
 
-			if ( is_home() && ! is_front_page() ) :
+			/*if ( is_home() && ! is_front_page() ) :
 				?>
 				<header>
 					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?> </h1>
 				</header>
 				<?php
-			endif;
+			endif;*/
 
 			/* Start the Loop */
 			while ( have_posts() ) :
