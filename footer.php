41f26a496954988664de7b40d6021da71bfc2741
Paolo <mabeabe28@gmail.com>
2018-05-22 21:50:27 +0100

    Cleaned Posts
    Page

--- footer.php
+++ footer.php
@@ -15,22 +15,20 @@
 
 	<footer id="footer" >
 		<div class="site-info">
-			You Had Me At
-			<!--<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'youhadmeat_theme' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'youhadmeat_theme' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'youhadmeat_theme' ), 'youhadmeat_theme', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>-->
+			 &copy; <?php echo date('Y'); ?> You Had Me At
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div> <!--#page -->
+<style>
 
+#footer {
+	height: 10vw;
+	font-family: "Avenir", "Open Sans";
+	color: white;
+}
+
+
+</style>
 <?php wp_footer(); ?>
 
 </body>
--- template-parts/content.php
+++ template-parts/content.php
@@ -10,27 +10,6 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php
-		if ( is_singular() ) :
-			the_title( '<h1 class="entry-title">', '</h1>' );
-		else :
-			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-		endif;
-
-		if ( 'post' === get_post_type() ) :
-			?>
-			<div class="entry-meta">
-				<?php
-				youhadmeat_theme_posted_on();
-				youhadmeat_theme_posted_by();
-				?>
-			</div><!-- .entry-meta -->
-		<?php endif; ?>
-	</header><!-- .entry-header -->
-
-	<?php youhadmeat_theme_post_thumbnail(); ?>
-
 	<div class="entry-content">
 		<?php
 		the_content( sprintf(
@@ -46,14 +25,13 @@
 			get_the_title()
 		) );
 
-		wp_link_pages( array(
+		/*wp_link_pages( array(
 			'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'youhadmeat_theme' ),
 			'after'  => '</div>',
-		) );
+		) );*/
 		?>
 	</div><!-- .entry-content -->
 
 	<footer class="entry-footer">
-		<?php youhadmeat_theme_entry_footer(); ?>
 	</footer><!-- .entry-footer -->
 </article><!-- #post-<?php the_ID(); ?> -->
