573b6329f754a11d99c6242335c72e18c839a924
Paolo <mabeabe28@gmail.com>
2018-05-27 11:34:22 +0100

    dont use image magick image processor

--- functions.php
+++ functions.php
@@ -165,3 +165,11 @@ require get_template_directory() . '/inc/customizer.php';
 if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
+
+function wpb_image_editor_default_to_gd( $editors ) {
+    $gd_editor = 'WP_Image_Editor_GD';
+    $editors = array_diff( $editors, array( $gd_editor ) );
+    array_unshift( $editors, $gd_editor );
+    return $editors;
+}
+add_filter( 'wp_image_editors', 'wpb_image_editor_default_to_gd' );
