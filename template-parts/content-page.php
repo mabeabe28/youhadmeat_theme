8df9a2e83e648af4bf02aa7bfa5c1c433284a119
Marion Abeabe <mabeabe28@gmail.com>
2018-05-29 16:18:29 +0100

    remove map...it aint free

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -60,43 +60,7 @@
 	      'post_status' => 'publish',
 	      'suppress_filters' => true
 	    );
-
-			$showMap = get_post_meta( $post->ID,'showMap', true );
-
-				if($showMap == 'true'){
-					echo '<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-							  <script>
-							    google.charts.load("current", { "packages": ["map"] });
-							    google.charts.setOnLoadCallback(drawMap);
-
-							    function drawMap() {
-							      var data = google.visualization.arrayToDataTable([
-							        ["Country", "Population"],
-							        ["China", "China: 1,363,800,000"],
-							        ["India", "India: 1,242,620,000"],
-							        ["US", "US: 317,842,000"],
-							        ["Indonesia", "Indonesia: 247,424,598"],
-							        ["Brazil", "Brazil: 201,032,714"],
-							        ["Pakistan", "Pakistan: 186,134,000"],
-							        ["Nigeria", "Nigeria: 173,615,000"],
-							        ["Bangladesh", "Bangladesh: 152,518,015"],
-							        ["Russia", "Russia: 146,019,512"],
-							        ["Japan", "Japan: 127,120,000"]
-							      ]);
-
-							    var options = {
-							      showTooltip: true,
-							      showInfoWindow: true
-							    };
-
-							    var map = new google.visualization.Map(document.getElementById("chart_div"));
-
-							    map.draw(data, options);
-									};
-									</script>
-									<div id="chart_div"></div>';
-				}
-
+			
 	    //get one recent post
 	    $recent_posts = wp_get_recent_posts( $args );
 	    //get the categories for the post
