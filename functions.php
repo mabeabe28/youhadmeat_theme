0a9463c16881e76759ed350baeaa56f38b0c8829
Marion Abeabe <mabeabe28@gmail.com>
2018-06-01 11:36:16 +0100

    initial save custom field

--- functions.php
+++ functions.php
@@ -272,7 +272,27 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 		$meta = get_post_meta( $post->ID );
 		?>
 
-		<label for="comingsoon"><?php _e( 'Example Text Input', 'yhma-textdomain' )?></label>
+		<label for="comingsoon">Coming Soon</label>
 		<input type="text" name="comingsoon" id="comingsoon" value="<?php if ( isset ( $meta['comingsoon'] ) ) echo $meta['comingsoon'][0]; ?>" />
 
 		<?php }
+
+		function custom_template_options_save( $post_id ) {
+
+		    // Checks save status
+		    $is_autosave = wp_is_post_autosave( $post_id );
+		    $is_revision = wp_is_post_revision( $post_id );
+		    $is_valid_nonce = ( isset( $_POST[ 'yhma_nonce' ] ) && wp_verify_nonce( $_POST[ 'yhma_nonce' ], basename( __FILE__ ) ) ) ? 'true' : 'false';
+
+		    // Exits script depending on save status
+		    if ( $is_autosave || $is_revision || !$is_valid_nonce ) {
+		        return;
+		    }
+
+		    // Checks for input and sanitizes/saves if needed
+		    if( isset( $_POST[ 'comingsoon' ] ) ) {
+		        update_post_meta( $post_id, 'comingsoon', sanitize_text_field( $_POST[ 'comingsoon' ] ) );
+		    }
+
+		}
+		add_action( 'save_post', 'custom_template_options_save' );
