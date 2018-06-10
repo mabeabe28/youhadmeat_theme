049b05295e69b25d20d5f2b0a50fb7eb8da5f46d
Paolo <mabeabe28@gmail.com>
2018-06-10 22:47:22 +0100

    add featured fade

--- front-page.php
+++ front-page.php
@@ -71,7 +71,7 @@ get_header();
 							}
 							$catLink = ''.get_site_url().'/'.$curcat->slug.'';
 
-							echo '  <div class="slide" style="background-image:linear-gradient(rgba(0, 0, 0, 0.5),rgba(0, 0, 0, 0.5)), url('.get_the_post_thumbnail_url($recent_post[0]["ID"], 'full').');"">
+							echo '  <div class="slide featured-fade" style="background-image:linear-gradient(rgba(0, 0, 0, 0.5),rgba(0, 0, 0, 0.5)), url('.get_the_post_thumbnail_url($recent_post[0]["ID"], 'full').');"">
 							<div style="display:none;">
 								<!--load image before hand,but hide display to prevent blank flashes when changing slide since background-image still be loading image-->
 								<img src="'.get_the_post_thumbnail_url($recent_post[0]["ID"], 'full').'">
