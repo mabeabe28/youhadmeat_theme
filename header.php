b8d7ea0ebe4f8fb653cd2a4df818cee9988dc2aa
Marion Abeabe <mabeabe28@gmail.com>
2018-07-09 10:52:49 +0100

    try import rather than link font

--- header.php
+++ header.php
@@ -17,7 +17,9 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 
-	<link href="https://fonts.googleapis.com/css?family=Crimson+Text|Open+Sans" rel="stylesheet">
+	<style>
+	@import url('https://fonts.googleapis.com/css?family=Crimson+Text|Open+Sans');
+	</style>
 	<script src="http://code.jquery.com/jquery-git.min.js"></script>
 
 	<?php wp_head(); ?>
