46d2f47f7f035bb99bd2f82c4c901360d36c0ade
Paolo <mabeabe28@gmail.com>
2019-01-11 00:40:58 +0000

    Tidy

--- search.php
+++ search.php
@@ -104,12 +104,7 @@ get_header();
 							</a>
 					</li>';
 
-				/**
-				 * Run the loop for the search to output the results.
-				 * If you want to overload this in a child theme then include a file
-				 * called content-search.php and that will be used instead.
-				 */
-				/*get_template_part( 'template-parts/content', 'search' );*/
+
 
 			endwhile;
 			echo 		'</ul>';//category-content
