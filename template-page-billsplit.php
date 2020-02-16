1e0973ccd31252c10fc36464d54d0e3b5e9ba105
Marion Paolo Abeabe <mabeabe28@gmail.com>
2020-02-16 20:09:26 +0000

    Add template page for billsplit react page

---
+++ template-page-billsplit.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Template Name: Community Page React
+ * Template Post Type:  page
+ *
+ * @package youhadmeat_theme
+ */
+
+get_header();
+?>
+<div id="root"></div>
+
+<script src="https://www.youhadme.at/wp-content/themes/youhadmeat_theme-master/includes/react-apps/billsplit/billsplit.js"></script>
+<link rel = "stylesheet" type = "text/css" href = "https://www.youhadme.at/wp-content/themes/youhadmeat_theme-master/includes/react-apps/billsplit/billsplit.css" />
+
+<script>
+    document.getElementById("site-navigation").style.display = "none";
+    document.getElementById("initial-navigation").style.display = "none";
+</script>
+<?php
+/*get_sidebar();*/
+get_footer();
