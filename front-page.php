2dcc56661567aaad7d62c9f802e208a9497aeda0
Marion Paolo Abeabe <mabeabe28@gmail.com>
2021-09-11 02:00:31 +0100

    Initial work done for new front page

--- front-page.php
+++ front-page.php
@@ -21,15 +21,84 @@ body{
   color: white;
 }
 </style>
+
+<div id="hero">
+	<div class="hero__content-wrapper">
+		<div class="hero__content">
+			<?php
+				$args = array(
+					'numberposts' => 1,
+					'orderby' => 'post_date',
+					'order' => 'DESC',
+					'post_type' => 'post',
+					'post_status' => 'publish',
+					'suppress_filters' => true
+				);
+				$recent_posts_all = wp_get_recent_posts( $args );
+				
+				foreach($recent_posts_all as $post){
+					$curPostTitle = $post["post_title"];
+					$curExcerptStr = (strlen($post["post_excerpt"]) > 500) ? substr($post["post_excerpt"],0,500).'...' :$post["post_excerpt"];
+					$curPostUrl = get_permalink($post["ID"]);
+					$comingsoon = get_post_meta( $post["ID"],'comingsoon', true );
+					if($comingsoon){
+						$curPostTitle = 'Coming Soon';
+						$curExcerptStr = 'Content Coming Soon';
+						$curPostUrl = '##';
+					}
+					$curCategory = get_the_category($post["ID"]);
+					$curParentCategory = "";
 	
+					foreach($curCategory as $cat){
+						if($cat->parent == 0){
+							$curParentCategory = $cat;
+						}
+					}
+					$authorName = get_the_author_meta('user_nicename',$post["post_author"]);
+					$authorUrl = ''.get_site_url().'/'.get_the_author_meta('user_nicename',$post["post_author"]).'';
+					$date = date_create($post["post_date"]);
+
+	
+					
+
+					echo '
+						<div class="hero__content__photo" style="background-image:linear-gradient(rgba(0, 0, 0, 0.2),rgba(0, 0, 0, 0.2)), url('.get_the_post_thumbnail_url($post["ID"], 'large').');">
+						</div>
+						<div class="hero__content__information">
+							<div class="hero__content__information__items">
+								<div class="pre-head">
+									<div class="latest">MOST RECENT</div>
+								</div>
+								<div class="title" >'.$curPostTitle.'</div>
+								<div class="excerpt">'.$curExcerptStr.'</div>
+								<div class="meta">
+									<div class="category">'.strtoupper($curParentCategory->name).'</div>
+									<div class="author"><i class="fa fa-user"></i>'.strtoupper($authorName).'</div>
+									<div class="date"><i class="fa fa-calendar"></i>'.date_format($date , 'd F Y').'</div>
+
+								</div>
+							</div>
+						</div>
+						
+					';
+	
+	
+				}
+				
+			?>
+		</div>
+	</div>
+</div>
+
 <div>
 
+	
 	<div class="frame">
 			<div class="frame__border">
 			</div>
 			<?php
 				$args = array(
-					'numberposts' => 4,
+					'numberposts' => 1,
 					'orderby' => 'post_date',
 					'order' => 'DESC',
 					'post_type' => 'post',
