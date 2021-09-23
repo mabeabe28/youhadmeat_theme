97dd0a35efd58886b9e12c3659b84df1679c0b29
Marion Paolo Abeabe <mabeabe28@gmail.com>
2021-09-23 01:18:09 +0100

    add mobile for front page

--- front-page.php
+++ front-page.php
@@ -107,7 +107,7 @@ body{
 										
 					
 										echo '
-											<div class="[ recent-item w-25 ]">			
+											<div class="[ recent-item ]">			
 												<a href="'.$curPostUrl.'" class="recent-item__information">
 														<div class="photo">
 														<div  class="image" style="background-image:linear-gradient(rgba(0, 0, 0, 0.2),rgba(0, 0, 0, 0.2)), url('.get_the_post_thumbnail_url($post["ID"], 'large').');">
@@ -133,75 +133,6 @@ body{
 			?>
 	</div>
 </div>
-
-<!-- <div id="recent">
-	<div class="recent__content-wrapper">
-		<div class="recent__content">
-		<?php
-				$args = array(
-					'numberposts' => 3,
-					'offset' => 1,
-					'orderby' => 'post_date',
-					'order' => 'DESC',
-					'post_type' => 'post',
-					'post_status' => 'publish',
-					'suppress_filters' => true
-				);
-				$recent_posts_all = wp_get_recent_posts( $args );
-				
-				foreach($recent_posts_all as $post){
-					$curPostTitle = $post["post_title"];
-					$curExcerptStr = (strlen($post["post_excerpt"]) > 80) ? substr($post["post_excerpt"],0,80).'...' :$post["post_excerpt"];
-					$curPostUrl = get_permalink($post["ID"]);
-					$comingsoon = get_post_meta( $post["ID"],'comingsoon', true );
-					if($comingsoon){
-						$curPostTitle = 'Coming Soon';
-						$curExcerptStr = 'Content Coming Soon';
-						$curPostUrl = '##';
-					}
-					$curCategory = get_the_category($post["ID"]);
-					$curParentCategory = "";
-	
-					foreach($curCategory as $cat){
-						if($cat->parent == 0){
-							$curParentCategory = $cat;
-						}
-					}
-					$authorName = get_the_author_meta('user_nicename',$post["post_author"]);
-					$authorUrl = ''.get_site_url().'/'.get_the_author_meta('user_nicename',$post["post_author"]).'';
-					$date = date_create($post["post_date"]);
-
-	
-					
-
-					echo '
-						<div class="recent-item">
-							<div class="recent-item__photo" style="background-image:linear-gradient(rgba(0, 0, 0, 0.2),rgba(0, 0, 0, 0.2)), url('.get_the_post_thumbnail_url($post["ID"], 'large').');">
-							</div>
-							<div class="recent-item__information">
-								<div class="recent-item__information__items">
-									
-									<div class="title" >'.$curPostTitle.'</div>
-									<div class="excerpt">'.$curExcerptStr.'</div>
-									<a href="'.$curPostUrl.'" class="read">READ MORE</a>
-									<div class="meta">
-										<div class="category">'.strtoupper($curParentCategory->name).'</div>
-										<div class="author"><i class="fa fa-user"></i>'.strtoupper($authorName).'</div>
-										<div class="date"><i class="fa fa-calendar"></i>'.date_format($date , 'd F Y').'</div>
-
-									</div>
-								</div>
-							</div>
-						</div>
-					';
-	
-	
-				}
-				
-			?>
-		</div>
-	</div>
-</div> -->
 
 <div class="main">
 	
